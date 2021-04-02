8cee6729a6d00e7ead4f6ddbeafc33d33033f883
Kafka Z <43937218+kafkaontheshore5997@users.noreply.github.com>
2021-04-02 12:30:02 -0400

    Create smallestDivisor.py

---
+++ smallestDivisor.py
@@ -0,0 +1,27 @@
+import math
+class Solution(object):
+    def smallestDivisor(self, nums, threshold):
+        """
+        :type nums: List[int]
+        :type threshold: int
+        :rtype: int
+        """
+        left, right = 1, max(nums)
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if self.ifLessThanThreshold(nums, mid, threshold):
+                right = mid
+            else:
+                left = mid
+        if self.ifLessThanThreshold(nums, left, threshold):
+            return left
+        return right
+    
+    def ifLessThanThreshold(self, nums, mid, threshold):
+        total = 0
+        for num in nums:
+            total += math.ceil(float(num) / mid)
+        return total <= threshold
+      
+# LC 1283
+# Binary search on the answer set 
